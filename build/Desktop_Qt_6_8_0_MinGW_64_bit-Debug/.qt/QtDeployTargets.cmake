40e89e45bca2cf8944921432dfd198775a6721b1
Arthur Norberto <arthurnorberto4@gmail.com>
2024-10-16 11:34:59 -0300

    initial commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AeroGestor_FILE C:/Users/Arthur/OneDrive/Documentos/AeroGestor/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/AeroGestor.exe)
+set(__QT_DEPLOY_TARGET_AeroGestor_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AeroGestor_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
